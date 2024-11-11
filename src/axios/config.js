5b0bf77e6a5f9b07a408899d6ee9786636cbc50b
Leyukarii <55262488+Leyukarii@users.noreply.github.com>
2024-11-11 19:29:55 -0300

    .env

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "/api"
-})
+    baseURL: import.meta.env.VITE_API_BASE_URL, // Usando a variável de ambiente
+});
 
 export default api;
