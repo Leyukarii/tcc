bff2191b0420f168f575c536183541a0a4200656
Leyukarii <55262488+Leyukarii@users.noreply.github.com>
2024-11-29 19:31:38 -0300

    imagem

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import SidebarMenu from "../Components/Sidebar/index";
 import styles from './Home.module.css';
 import { AuthContext } from '../context/AuthContext';
+import redux from '../Assets/redux.png';
 
 const Home = () => {
   const { user } = useContext(AuthContext); // Obtém o usuário do contexto
@@ -13,7 +14,7 @@ const Home = () => {
         <h1 className={styles.titulo}>Seja Bem Vindo ao EFarma</h1>
         <h2 className={styles.h2}>Gestão Inteligente de Medicamentos</h2>
         <p className={styles.h2}>Cargo: {user?.role || "Usuário não logado"}</p> {/* Exibe o cargo do usuário */}
-        <img className={styles.imagem} src="src/Assets/redux.png" alt="Descrição da Imagem"/>
+        <img className={styles.imagem} src={src/Assets/redux.png} alt="Descrição da Imagem"/>
       </div>
     </div>
   );
